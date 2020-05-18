646104db91a7c1da9532309100dc592d4ba74773
richen <richenlin@gmail.com>
2020-05-18 16:40:03 +0800

    think_crypto@1.0.0

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @ author: richen
+ * @ copyright: Copyright (c) - <richenlin(at)gmail.com>
+ * @ license: MIT
+ * @ version: 2020-05-18 16:35:04
+ */
+
+module.exports = {
+    aes: require('./lib/aes'),
+    des: require('./lib/des'),
+    rsa: require('./lib/rsa')
+}; 
